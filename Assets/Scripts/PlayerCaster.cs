0aa067b72924f9cf95409c49eff954af7310e9d6
Zachary Cohen <zachary@computingfun.org>
2019-09-14 18:18:22 -0400

    Start player input and movement

---
+++ Assets/Scripts/PlayerCaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCaster:MonoBehaviour {
+
+    private Vector2 input;
+
+    private void Awake() {
+        InputMain.GetOrCreate().Player.Look.performed += LookPerformed;
+    }
+
+    private void LookPerformed(InputAction.CallbackContext ctx) =>
+        input = ctx.ReadValue<Vector2>();
+
+    private void Update() {
+        if (input == Vector2.zero) {
+            return;
+        }
+
+        // TODO: cast spell
+    }
+}
